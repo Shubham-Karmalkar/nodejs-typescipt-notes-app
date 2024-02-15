a10d2fa34f32c4c908e0e26144f7de60e8d90d87
Karmalkar Shubham <shubham.karmalkar@incred.com>
2024-02-15 23:30:04 +0530

    feat(user module): integrate userget with email api in user module

--- src/apps/user/user.module.ts
+++ src/apps/user/user.module.ts
@@ -1,12 +1,15 @@
-import { Providers } from "./types";
+import { Database } from "@root/db";
+import { User } from "./types";
+import { config } from "@root/config";
+export class UserModule {
+  private db: Database;
+  constructor() {
+    this.db = new Database(config["table_mapping"]["user_data"]);
+  }
+  async getUserByEmail(email: string): Promise<User> {
+    const user = await this.db.get(email);
+    console.log("user: ", user);
+    return user as User;
+  }
 
-export interface User {
-  _id: string;
-  name: string;
-  email: string;
-  imageUrl: string;
-  createAt: number;
-  updatedAt: number;
-  providerId: Providers;
-  phoneNumber: number;
 }
--- src/apps/user/user.service.ts
+++ src/apps/user/user.service.ts
@@ -1,1 +1,15 @@
-class UserService {}
+import { UserModule } from "./user.module";
+import { Exception } from "@root/core";
+
+export class UserService {
+  private userModule: UserModule;
+
+  constructor() {
+    this.userModule = new UserModule();
+  }
+
+  async getUserByEmail(email?: string) {
+    if(!email) throw new Exception("Can not get user with empty Email id");
+    return this.userModule.getUserByEmail(email);
+  }
+}
