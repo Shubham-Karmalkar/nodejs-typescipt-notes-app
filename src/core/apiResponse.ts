e823177f1de49254b899fbb63bd7002ff505a4f0
Karmalkar Shubham <shubham.karmalkar@incred.com>
2024-03-23 00:34:38 +0530

    refactor(core): improve api response and error handlers

--- src/core/apiResponse.ts
+++ src/core/apiResponse.ts
@@ -1,12 +1,16 @@
 import { HttpStatus } from "../constants";
 import { ValuesOf } from "../types";
 
-export class ApiResponse {
+export class ApiResponse<Type = any> {
   public status = true;
   public statusCode = 200;
-  public response: any;
-  constructor(response: any, statusCode?: ValuesOf<typeof HttpStatus>) {
+  public response: Type;
+  constructor(response: Type, statusCode?: ValuesOf<typeof HttpStatus>) {
     this.response = response;
     if (statusCode) this.statusCode = statusCode;
   }
+}
+
+export function apiResponse<Type>(response: Type, statusCode?: ValuesOf<typeof HttpStatus>) {
+  return new ApiResponse(response, statusCode);
 }
--- src/core/errors/handlers.ts
+++ src/core/errors/handlers.ts
@@ -1,17 +1,40 @@
 import { Response } from "express";
 import { Exception } from "./apiErrors";
+import { HttpStatus } from "@root/constants";
+import { HasPropOf } from "@root/types";
 
 type ErrorTypes = "appError" | "unhandledRejection" | "uncaughtException" | "unKnown";
 
 class ErrorHandler {
-  public async handleError(error: Error, type: ErrorTypes, res?: Response): Promise<void> {
-    console.error("inside handleError: ", error, type);
+  public async handleError(error: Error, type: ErrorTypes, res?: Response) {
+    if (type === "appError" && this.isTrustedError(error)) {
+      return res?.status(error.statusCode).json(this.formatErrorResponse(error));
+    }
+    console.error(error);
+
+    if (type === "unKnown") {
+      const errResponse = this.formatErrorResponse(error);
+      res?.status(errResponse.statusCode).json(errResponse);
+    }
+    process.exit(1);
     /**
      * await logger.logError(err);
     await sendMailToAdminIfCritical();
     await saveInOpsQueueIfCritical();
     await determineIfOperationalError();
      */
+  }
+
+  public formatErrorResponse(error: Exception | Error): ErrorResp {
+    if (error instanceof Exception) {
+      return { ...error, message: error.message };
+    }
+    return {
+      status: false,
+      isOperational: false,
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: error.message || (error.stack ? JSON.stringify(error.stack) : JSON.stringify(error)),
+    };
   }
 
   public isTrustedError(error: Error): error is Exception {
@@ -21,5 +44,6 @@ class ErrorHandler {
     return false;
   }
 }
+type ErrorResp = Omit<HasPropOf<Exception>, "name">;
 
 export const errorHandler = new ErrorHandler();
