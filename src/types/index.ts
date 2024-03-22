be85a8e388e1701432af62157bf8c86b727ef287
Karmalkar Shubham <shubham.karmalkar@incred.com>
2024-03-23 00:36:00 +0530

    feat(types): add new type in utilty types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export type VoidFunc = (...args: any[]) => void;
 
-export type Func<Type> = (...args: any[]) => Type;
+export type Func<Type, rType = any> = (args: Type) => rType;
 
 export type AsyncFunc<Type> = (...args: any[]) => Promise<Type>;
 
@@ -11,4 +11,8 @@ export type ValuesOf<Type> = Type[keyof Type];
 //Objects
 export type GenericObj<Type = any> = {
   [prop: string]: Type;
+};
+
+export type HasPropOf<Type extends GenericObj> = {
+  [Property in keyof Type]: any;
 };
